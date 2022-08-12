b846aa4e81e676a47c012f5c12bcf8745183e61c
Bradbuchholz <bradbuchholz@gmail.com>
2022-08-12 12:42:32 -0700

    update models

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -1,2 +1,17 @@
 using System;
 using System.Collections.Generic;
+
+namespace Factory.Models
+{
+  public class Engineer
+  {
+    public Engineer()
+    {
+      this.JoinEntities = new HashSet<EngineerMachine>();
+    }
+    public int EngineerId { get; set; }
+    public string Name { get; set; }
+    public DateTime HireDate { get; set; }
+    public virtual ICollection<EngineerMachine> JoinEntities { get; set; }
+  }
+}
