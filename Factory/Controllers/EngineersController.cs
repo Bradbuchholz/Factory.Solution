a1910d6134c16b468e5cd2c641e70c6720461656
Bradbuchholz <bradbuchholz@gmail.com>
2022-08-12 10:52:04 -0700

    set up basic using statments

---
+++ Factory/Controllers/EngineersController.cs
@@ -0,0 +1,5 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Factory.Models;
+using System.Linq;
---
+++ Factory/Migrations/20220324215740_Initial.Designer.cs
@@ -0,0 +1,3 @@
+using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
